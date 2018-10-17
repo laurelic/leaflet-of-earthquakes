0e0d783b6695042263dcd02cd6ed5da5f4112b6f
laurelic <37823073+laurelic@users.noreply.github.com>
2018-10-17 16:49:53 -0700

    earthquake markers draft

--- js/logic.js
+++ js/logic.js
@@ -2,34 +2,91 @@
 var qUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 
-//Create a satellite view layer
-var satelliteLayer =     L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.satellite",
-    accessToken: API_KEY
+d3.json(qUrl, function(data) {
+    createBubbles(data.features);
+    // centerMap(data);
+
 });
 
-// // Perform a GET request to the query URL
-var mapCenter = d3.json(qUrl, function(data) {
+//build a function for centering the map
+// function centerMap(data) {
+//     var corner1 = L.latLng(data.bbox[1], data.bbox[0]),
+//     corner2 = L.latLng(data.bbox[4], data.bbox[3]),
+//     mc = Object.values(L.latLngBounds(corner1, corner2).getCenter());
 
-    //Get the center of the map for the data
-    var corner1 = L.latLng(data.bbox[1], data.bbox[0]),
-    corner2 = L.latLng(data.bbox[4], data.bbox[3]),
-    mc = Object.values(L.latLngBounds(corner1, corner2).getCenter());
-    
-    console.log(mc);
+//     return mc;
+// };
 
-    var myMap = L.map("map-id", {
-        center: mc,
-        zoom: 3
+// var mapCenter = d3.json(qUrl, function(data) {
+//     var corner1 = L.latLng(data.bbox[1], data.bbox[0]),
+//     corner2 = L.latLng(data.bbox[4], data.bbox[3]),
+//     mc = Object.values(L.latLngBounds(corner1, corner2).getCenter());
+//     console.log(mc)
+//     return mc;
+// });
+
+function createBubbles(data) {
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<p>" + feature.properties.title + "</p>");
+    }
+
+    var quakes = L.geoJSON(data, {
+        onEachFeature: onEachFeature
     });
 
-    satelliteLayer.addTo(myMap);
+    createMap(quakes);
+}
 
-});
+function createMap(quakes) {
 
-console.log(mapCenter);
+//initialize the layergroups we will use
+// var layers = {
+//     tier0: new L.LayerGroup(),
+//     tier1: new L.LayerGroup(),
+//     tier2: new L.LayerGroup(),
+//     tier3: new L.LayerGroup(),
+//     tier4: new L.LayerGroup(),
+//     tier5: new L.LayerGroup(),
+// };
+
+    //Create a satellite view layer
+    var satelliteLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.satellite",
+        accessToken: API_KEY
+    });
+
+    //Create a satellite view layer
+    var lightLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.light",
+        accessToken: API_KEY
+    });
+
+    var baseMaps = {
+        "Satellite View": satelliteLayer,
+        "Outline View": lightLayer
+    };
+
+    var quakeLayer = {
+        Earthquakes: quakes
+    };
+// // Perform a GET request to the query URL
+
+    var map = L.map("map-id", {
+        center: [5.2, -0.06],
+        zoom: 3,
+        layers: [satelliteLayer, quakes]
+    });
+
+    L.control.layers(baseMaps, quakeLayer, {
+        collapsed: false
+    }).addTo(map);
+    
+};
+
 
 
 
